162f1584aac68122ad3eba85bcfc45406ebf2672
indhu10244-spec <indhu10244@gmail.com>
2026-04-25 15:53:14 +0530

    Refactor input and print statements in 1a  Arithmetic Op.py

--- 1a.py
+++ 1a.py
@@ -1,20 +1,9 @@
-Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> a=int(input("Enter a number:"))
-Enter a number:10
->>> b=int(input("Enter a second number:"))
-Enter a second number:3
->>> print("Add:",a+b)
-Add: 13
->>> print("Sub:",a-b)
-Sub: 7
->>> print("Multiplication:",a*b)
-Multiplication: 30
->>> print("Division:",a/b)
-Division: 3.3333333333333335
->>> print("Modulus:",a%b)
-Modulus: 1
->>> print("Exponentation:",a**b)
-Exponentation: 1000
->>> print("Floor Division:",a//b)
-Floor Division: 3
+a=int(input("Enter a number:"))
+b=int(input("Enter a second number:"))
+print("Add:",a+b)
+print("Sub:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+print("Modulus:",a%b)
+print("Exponentation:",a**b)
+print("Floor Division:",a//b)
